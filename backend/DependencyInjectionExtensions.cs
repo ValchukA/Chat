2c9ace1110e0b6eb47510e2f47b32084f3c5d46b
ValchukA <andva02@gmail.com>
2024-02-18 03:48:43 +0300

    Add initial project files

---
+++ backend/DependencyInjectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Chat.Api;
+
+internal static class DependencyInjectionExtensions
+{
+    public static void AddApiServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen();
+    }
+}
